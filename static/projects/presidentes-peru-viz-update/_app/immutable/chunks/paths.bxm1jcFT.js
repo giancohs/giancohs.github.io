f6e37c5e12b48f7bd16b3347d2299bedd72889cd
giancohs <giancohs@gmail.com>
2026-02-17 16:37:22 +0000

    updating viz presidentes

---
+++ static/projects/presidentes-peru-viz-update/_app/immutable/chunks/paths.bxm1jcFT.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1gswvv6)==null?void 0:s.base)??"/static/projects/presidentes-peru-viz";var e;const a=((e=globalThis.__sveltekit_1gswvv6)==null?void 0:e.assets)??t;export{a,t as b};
